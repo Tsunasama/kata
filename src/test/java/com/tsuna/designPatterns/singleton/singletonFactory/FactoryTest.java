efab7c6992b32ddd8f947baaad2fe5b882cf5806
Tsuna <DisciplineManager@outlook.com>
2017-10-29 21:03:17 +0800

    Repair the bug in FactoryTest.

--- src/test/java/com/tsuna/designPatterns/singleton/singletonFactory/FactoryTest.java
+++ src/test/java/com/tsuna/designPatterns/singleton/singletonFactory/FactoryTest.java
@@ -11,15 +11,32 @@ public class FactoryTest {
         Processing p1 = new Processing();
         Processing p2 = new Processing();
         Processing p3 = new Processing();
-        p1.run();
-        p2.run();
-        p3.run();
+        Thread t1=new Thread(p1);
+        Thread t2=new Thread(p2);
+        Thread t3=new Thread(p3);
+        //Make threee thread running concurrently
+        t1.start();
+        t2.start();
+        t3.start();
+        try {
+            Thread.sleep(100);
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
         Assert.assertEquals(3, Factory.getFactory().getCounts());
+        Assert.assertSame(p1.getFactory(),p2.getFactory());
+        Assert.assertSame(p1.getFactory(),p3.getFactory());
     }
 
     class Processing implements Runnable {
+        private Factory factory;
+
+        Factory getFactory() {
+            return factory;
+        }
+
         public void run() {
-            Factory factory = Factory.getFactory();
+            this.factory = Factory.getFactory();
             factory.duDuty();
         }
     }
