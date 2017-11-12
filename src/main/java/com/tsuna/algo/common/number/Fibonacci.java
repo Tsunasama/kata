93d5547c91c1296791a3b5541b058275b288bdb3
Tsuna <DisciplineManager@outlook.com>
2017-11-12 10:21:22 +0800

    Implements Fibonacci with lambda.

---
+++ src/main/java/com/tsuna/algo/common/number/Fibonacci.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017.
+ * Project : kata
+ * File : Fibonacci.java
+ * Date : 17-11-12 上午9:52
+ * Tsunasama All rights reserved.
+ */
+
+package com.tsuna.algo.common.number;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+class Fibonacci {
+    static Integer[] getFiboWithLambda(int count) {
+        if (count < 1)
+            throw new IllegalArgumentException("parameter:count cannot less than 1");
+        List<Integer> result = new ArrayList<>();
+        Stream.iterate(
+                new int[]{0, 1},
+                i -> new int[]{i[1], i[0] + i[1]})
+                .limit(count)
+                .forEach(i -> result.add(i[0]));
+        return result.toArray(new Integer[count]);
+    }
+}
---
+++ src/test/java/com/tsuna/algo/common/number/FibonacciTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2017.
+ * Project : kata
+ * File : FibonacciTest.java
+ * Date : 17-11-12 上午10:13
+ * Tsunasama All rights reserved.
+ */
+
+package com.tsuna.algo.common.number;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FibonacciTest {
+    @Test
+    public void getFiboWithLambda() throws Exception {
+        Integer[] result = Fibonacci.getFiboWithLambda(20);
+        int[] expected = new int[]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181};
+        for (int i = 0; i < result.length; i++) {
+            Assert.assertEquals(expected[i], result[i].intValue());
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getFiboWithWrongParameter() {
+        Fibonacci.getFiboWithLambda(0);
+    }
+}
