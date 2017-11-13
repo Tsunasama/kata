1418f43fe01708c2f20f7ab25b612ce8d0b3e704
Tsuna <DisciplineManager@outlook.com>
2017-11-13 21:33:23 +0800

    Implement sieve of eratosthenes using parallel computing with lambda.

--- src/main/java/com/tsuna/algo/common/number/SieveOfEratosthenes.java
+++ src/main/java/com/tsuna/algo/common/number/SieveOfEratosthenes.java
@@ -9,7 +9,9 @@
 package com.tsuna.algo.common.number;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 class SieveOfEratosthenes {
     static List<Integer> find(int topLimit) {
@@ -24,7 +26,7 @@ class SieveOfEratosthenes {
         for (int i = 2; i <= max; i++) {
             result.add(i);
         }
-        while (current < Math.sqrt(max)) {
+        while (current <= Math.sqrt(max)) {
             result = cleanMultiplication(result, current);
             max = result.get(result.size() - 1);
             current++;
@@ -43,6 +45,47 @@ class SieveOfEratosthenes {
                 result.add(values.get(index));
             index++;
         }
+        return result;
+    }
+
+    /**
+     * @param limit:the max number
+     * @return boolean array,of which indexes indicate the checked number,and array content shows if the index number is prime factor
+     */
+    static boolean[] findUsingBoolean(int limit) {
+        if (limit < 1)
+            throw new IllegalArgumentException("Parameter:limit cannot less than 1");
+        boolean[] results = new boolean[limit + 1];
+        for (int i = 2; i <= limit; i++)
+            results[i] = true; // initial all to true
+        for (int i = 2; i <= Math.sqrt(limit); i++) {
+            for (int j = i + 1; j <= limit; j++)
+                if (results[i])
+                    if (j % i == 0)
+                        results[j] = false;
+        }
+        return results;
+    }
+
+    /**
+     * Using lambda to parallel compute
+     *
+     * @param limit:max number
+     * @return result array
+     */
+    static int[] findUsingLambda(int limit) {
+        if (limit < 1)
+            throw new IllegalArgumentException("Parameter:limit cannot less than 1");
+        int range = (int) Math.sqrt(limit);
+        int[] result = IntStream.rangeClosed(2, limit).parallel().filter(i -> {
+            if (i == 2) {
+                return true;
+            }
+            if (i <= range) {
+                return IntStream.rangeClosed(2, i - 1).noneMatch(j -> i % j == 0);
+            }
+            return IntStream.rangeClosed(2, range).noneMatch(j -> i % j == 0);
+        }).toArray();
         return result;
     }
 }
--- src/test/java/com/tsuna/algo/common/number/SieveOfEratosthenesTest.java
+++ src/test/java/com/tsuna/algo/common/number/SieveOfEratosthenesTest.java
@@ -18,7 +18,7 @@ public class SieveOfEratosthenesTest {
     @Test
     public void testFind() throws Exception {
         List<Integer> result = SieveOfEratosthenes.find(25);
-        Assert.assertEquals("[2, 3, 5, 7, 11, 13, 17, 19, 23, 25]", result.toString());
+        Assert.assertEquals("[2, 3, 5, 7, 11, 13, 17, 19, 23]", result.toString());
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -32,4 +32,19 @@ public class SieveOfEratosthenesTest {
         Assert.assertEquals("[2, 3, 4, 5, 7, 8, 11]", result.toString());
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindUsingLambdaWithErrorParameter() {
+        SieveOfEratosthenes.findUsingLambda(0);
+    }
+
+    @Test
+    public void testFindUsingLambda() {
+        int[] expected = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23};
+        Assert.assertArrayEquals(expected, SieveOfEratosthenes.findUsingLambda(25));
+    }
+
+    @Test(timeout = 2000)
+    public void testLambdaRunningTime() {
+        int[] result = SieveOfEratosthenes.findUsingLambda(1000000);
+    }
 }
